475d024544e9a5ab4bc39ae598e84cd1933bd1b5
erick <erickpanni@icloud.com>
2023-02-01 18:30:37 -0300

    CHORE[Web]: Create component header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,10 @@
+import { Header } from "./components/Header";
 import "./styles/global.css";
 
 export function App() {
-  return <div></div>;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Header />
+    </div>
+  );
 }
