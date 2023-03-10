8ce2b07babfcf7ef4c05c26ffba86fb26a9f370f
Erick <erickpanni@icloud.com>
2023-03-10 17:45:44 -0300

    CHORE[mobile]: Creating checkbox + new Habit button screen

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,11 +1,62 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { ScrollView, Text, TextInput, View } from "react-native";
+import { BackButton } from "../components/BackButton";
+import { CheckBox } from "../components/CheckBox";
+
+const availableWeekDays = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 export function New() {
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function handleToggleWeekDay(weekDayIndex: number) {
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays((prevState) =>
+        prevState.filter((weekDay) => weekDay !== weekDayIndex)
+      );
+    } else {
+      setWeekDays((prevState) => [...prevState, weekDayIndex]);
+    }
+  }
   return (
     <View
       className="flex-1 bg-background px-8 pt-16
     "
-    ></View>
+    >
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <BackButton />
+        <Text className="mt-6 text-white font-extrabold text-3xl">
+          Create habit
+        </Text>
+
+        <Text className="mt-6 text-white font-semibold text-base">
+          What is your commitment?
+        </Text>
+
+        <TextInput
+          className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-400
+        "
+        />
+        <Text className="front-semibold mt-4 mb-3 text-white text-base">
+          What is the recurrence?
+        </Text>
+
+        {availableWeekDays.map((weekDay, index) => (
+          <CheckBox
+            key={weekDay}
+            title={weekDay}
+            checked={weekDays.includes(index)}
+            onPress={() => handleToggleWeekDay(index)}
+          />
+        ))}
+      </ScrollView>
+    </View>
   );
 }
